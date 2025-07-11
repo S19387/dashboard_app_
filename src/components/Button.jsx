f0353db2508523d775c894de7f39b8200845b6ea
S19387 <dasuni2018@gmail.com>
2025-07-11 08:16:30 -0700

    created button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+// src/components/Button/Button.jsx
+import React from 'react';
+
+function Button({ label, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 focus:outline-none"
+    >
+      {label}
+    </button>
+  );
+}
+
+export default Button;
